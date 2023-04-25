f563da49143b4294ebb36d367955228fbae19633
mauro saavedra <saa.mauro92@gmail.com>
2023-04-26 00:46:25 +0100

    add cors in order to get requests from front

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("./middleware/bodyParser");
+const cors = require("cors");
 bodyParser(app);
 const routes = require("./routes");
 
-
+app.use(cors());
 app.use("/", routes);
 
 

dd41f9e6c004b51562245b210543cb5a2b00d50f
mauro saavedra <saa.mauro92@gmail.com>
2023-04-25 21:45:32 +0100

    add route with country as params

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const controllers = require("../controllers");
 const route = express.Router();
+
+route.get("/country/:country", controllers.getCountryData);
 
 module.exports = route;
